e743631e417dfaa70c1c88aa9f5fdb6a09f95f63
poggy33 <33poggy33@gmail.com>
2024-07-03 20:21:25 +0100

    Login Signup css

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import axios  from "axios";
+import axios from "axios";
 import toast from "react-hot-toast";
 
 export default function LoginPage() {
@@ -19,9 +19,9 @@ export default function LoginPage() {
       setLoading(true);
       const response = await axios.post("/api/users/login", user);
       console.log("Login success", response.data);
-      toast.success("Login success")
-      router.push("/profile")
-      router.refresh()
+      toast.success("Login success");
+      router.push("/profile");
+      router.refresh();
     } catch (error: any) {
       console.log("Login failed", error.message);
       toast.error(error.message);
@@ -31,10 +31,7 @@ export default function LoginPage() {
   };
 
   useEffect(() => {
-    if (
-      user.email.length > 0 &&
-      user.password.length > 0 
-    ) {
+    if (user.email.length > 0 && user.password.length > 0) {
       setButtonDisabled(false);
     } else {
       setButtonDisabled(true);
@@ -42,29 +39,58 @@ export default function LoginPage() {
   }, [user]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>{loading ? "Зачекайте" : "Увійти"}</h1>
+    <div className="flex flex-col bg-indigo-100 items-center min-h-screen py-2">
+      <div className="mt-10 mb-5">
+        {loading ? (
+          <div role="status">
+            <svg
+              aria-hidden="true"
+              className="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+        ) : (
+          <h1 className="text-xl">Вхід</h1>
+        )}
+      </div>
       <hr></hr>
-      <label htmlFor="email">email</label>
+      {/* <label htmlFor="email">email</label> */}
       <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="w-80 h-12 p-2 border border-gray-300 rounded-lg mb-8 focus:outline-none focus:border-gray-600"
         id="email"
         type="text"
         value={user.email}
         onChange={(e) => setUser({ ...user, email: e.target.value })}
         placeholder="email"
       />
-      <label htmlFor="password">password</label>
+      {/* <label htmlFor="password">password</label> */}
       <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 w-80 h-12 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
         id="password"
         type="password"
         value={user.password}
         onChange={(e) => setUser({ ...user, password: e.target.value })}
         placeholder="password"
       />
-      <button onClick={onLogin} className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Login</button>
-      <Link href='/signup'>Visit signup</Link>
+      <button
+        onClick={onLogin}
+        className="border-white w-40 mb-8 rounded-lg p-3 text-white bg-black mt-4 hover:bg-slate-700"
+      >
+        Увійти
+      </button>
+      <Link className="text-lg text-slate-700 underline" href="/signup">Реєстрація</Link>
     </div>
   );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
-import  axios  from "axios";
+import axios from "axios";
 
 export default function SignupPage() {
   const router = useRouter();
@@ -21,7 +21,7 @@ export default function SignupPage() {
       setLoading(true);
       const response = await axios.post("/api/users/signup", user);
       console.log("Response success", response.data);
-      router.push("/login")
+      router.push("/login");
     } catch (error: any) {
       console.log(error.message);
       toast.error(error.message);
@@ -44,39 +44,63 @@ export default function SignupPage() {
   }, [user]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>{loading ? "Зачекайте..." : "Реєстрація"}</h1>
+    <div className="flex flex-col bg-indigo-100 items-center min-h-screen py-2">
+      <div className="mt-10 mb-5">
+        {loading ? (
+          <div role="status">
+            <svg
+              aria-hidden="true"
+              className="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+        ) : (
+          <h1 className="text-xl">Реєстрація</h1>
+        )}
+      </div>
       <hr></hr>
-      <label htmlFor="username">username</label>
+      {/* <label htmlFor="username">username</label> */}
       <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="w-80 h-12 p-2 border border-gray-300 rounded-lg mb-8 focus:outline-none focus:border-gray-600"
         id="username"
         type="text"
         value={user.username}
         onChange={(e) => setUser({ ...user, username: e.target.value })}
         placeholder="username"
       />
-      <label htmlFor="email">email</label>
+      {/* <label htmlFor="email">email</label> */}
       <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="w-80 h-12 p-2 border border-gray-300 rounded-lg mb-8 focus:outline-none focus:border-gray-600"
         id="email"
         type="text"
         value={user.email}
         onChange={(e) => setUser({ ...user, email: e.target.value })}
         placeholder="email"
       />
-      <label htmlFor="password">password</label>
+      {/* <label htmlFor="password">password</label> */}
       <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="w-80 h-12 p-2 border border-gray-300 rounded-lg mb-8 focus:outline-none focus:border-gray-600"
         id="password"
         type="password"
         value={user.password}
         onChange={(e) => setUser({ ...user, password: e.target.value })}
         placeholder="password"
       />
-      <label htmlFor="phone">phone</label>
+      {/* <label htmlFor="phone">phone</label> */}
       <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="w-80 h-12 p-2 border border-gray-300 rounded-lg mb-8 focus:outline-none focus:border-gray-600"
         id="phone"
         type="text"
         value={user.phone}
@@ -85,11 +109,11 @@ export default function SignupPage() {
       />
       <button
         onClick={onSignup}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="border-white w-40 mb-8 rounded-lg p-3 text-white bg-black mt-4 hover:bg-slate-700"
       >
         {/* {buttonDisabled ? "Зареєструватися" : ""} */}Зареєструватися
       </button>
-      <Link href="/login">Visit login</Link>
+      <Link className="text-lg text-slate-700 underline" href="/login">Увійти</Link>
     </div>
   );
 }
