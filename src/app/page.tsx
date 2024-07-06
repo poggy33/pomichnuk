6e686d4d774ee009f896929a9fbb4b60b8a8baa2
poggy33 <33poggy33@gmail.com>
2024-07-06 17:29:51 +0100

    css main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Main from "@/components/Main";
 
 export default function Home() {
   return (
-    <div className="pt-10">
+    <div className="pt-4">
       <main className="flex justify-center">
         <Main />
       </main>
