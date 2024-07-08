47b6ce0e4dbca5560e25076abed48c35e06d103c
poggy33 <33poggy33@gmail.com>
2024-07-08 21:14:28 +0100

    some css navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,31 +44,31 @@ export default function Navbar() {
       <div className="bg-gradient-to-t from-yellow-300 to-blue-400 flex h-16 justify-between items-center pl-4">
         <div>
           <Link
-            className="bg-gradient-to-t from-yellow-300 to-blue-400 hover:to-blue-500 border h-8 max-sm:text-xs rounded-lg p-2"
+            className=""
             href={"/"}
           >
-            Головна
+            <span className="bg-gradient-to-t from-yellow-300 to-blue-400 hover:to-blue-500 border-2 hover:border-white h-8 max-sm:text-xs rounded-lg px-2 py-2">Головна</span>
           </Link>
         </div>
         <div className="flex pr-4">
           <button
             onClick={redirectProfile}
-            className="bg-gradient-to-t from-yellow-300 to-blue-400 hover:to-blue-500 border max-sm:text-xs rounded-lg px-2 py-1"
+            className=""
           >
-            Надати послугу
+            <span className="bg-gradient-to-t from-yellow-300 to-blue-400 hover:to-blue-500 border-2 hover:border-white h-8 max-sm:text-xs rounded-lg px-2 py-2">Надати послугу</span>
           </button>
 
           {isLoggedIn ? (
             <button
               onClick={logout}
-              className="bg-gradient-to-t from-yellow-300 to-blue-400 hover:to-blue-500 h-8 max-sm:text-xs ml-4 w-20 border rounded-lg px-2 py-1"
+              className=""
             >
-              Вийти
+              <span className="bg-gradient-to-t from-yellow-300 to-blue-400 hover:to-blue-500 border-2 hover:border-white h-8 max-sm:text-xs rounded-lg px-2 py-2 ml-4">Вийти</span>
             </button>
           ) : (
             <button
               onClick={() => router.push("/login")}
-              className="bg-gradient-to-t from-yellow-300 to-blue-400 hover:to-blue-500 border h-8 max-sm:text-xs ml-4 w-20 rounded-lg px-2 py-1"
+              className="bg-gradient-to-t from-yellow-300 to-blue-400 hover:to-blue-500 border-2 hover:border-white h-8 max-sm:text-xs rounded-lg px-2 py-2 ml-4"
             >
               Вхід
             </button>
@@ -76,7 +76,7 @@ export default function Navbar() {
         </div>
       </div>
       <div className="flex justify-center mt-4">
-        <h1 className="py-2 px-4 rounded-full text-xl bg-gradient-to-r from-blue-300 via-green-300 to-blue-200">ЄПоміч</h1>
+        <h1 className="py-2 px-4 rounded-full text-xl bg-gradient-to-r from-blue-300 via-green-300 to-blue-200 border-2 border-white">ЄДопомога</h1>
       </div>
     </>
   );
