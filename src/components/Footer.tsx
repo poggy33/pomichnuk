6f5f33755257faa6acf1b051242d9b97e9fdc17a
poggy33 <33poggy33@gmail.com>
2024-07-12 14:59:01 +0100

    edit footer 2 time

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,9 @@ import React from "react";
 
 function FooterPage() {
   return (
-    <div className="flex items-center justify-evenly text-xs text-gray-600 bg-gradient-to-b from-gray-200 to-gray-300 min-w-full min-h-10 border-white border-t-2 mt-4">
-
-        <span>© 2024 Created by Ihor.</span>
-        <span>Contacts: epomichnik@gmail.com</span>
-   
-      
+    <div className="px-4 flex items-center justify-between border-white border-t-2 text-xs bg-gradient-to-b from-gray-200 to-gray-300 min-w-full min-h-10  mt-4">
+      <span>© 2024 Created by Ihor.</span>
+      <span>Contacts: epomichnik@gmail.com</span>
     </div>
   );
 }
