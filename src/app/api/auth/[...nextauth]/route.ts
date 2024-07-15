924d361a39bc0e3988bac11b6762d3d4bb89964f
poggy33 <33poggy33@gmail.com>
2024-07-15 17:44:32 +0100

    google auth 5

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,7 @@ import GoogleProvider from  "next-auth/providers/google";
 const authOptions = {
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientId: process.env.GOOGLE_CLIENT_ID as string ,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
     ],
