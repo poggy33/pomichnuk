bf22e2ee26dde66a2fc128ace8b89fd8cc3eebf0
poggy33 <33poggy33@gmail.com>
2024-07-16 19:59:24 +0100

    google auth 8

--- src/components/LoginButtonGoogle.tsx
+++ src/components/LoginButtonGoogle.tsx
@@ -2,14 +2,22 @@
 
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
-import {signIn} from "next-auth/react";
+import { signIn } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 function LoginButtonGoogle() {
+    const {status, data: session} = useSession();
+    console.log(status);
+    console.log(session?.user?.email);
+
   return (
     <div className="flex flex-col mt-20">
       <p className="text-md mb-1">Увійти за допомогою Google</p>
       <div className="flex justify-center">
-        <div onClick={() => signIn("google")} className="flex justify-between w-40 items-center p-2 bg-slate-400 border-2 hover:border-white rounded-lg text-white">
+        <div
+          onClick={() => signIn("google")}
+          className="flex justify-between w-40 items-center p-2 pr-4 bg-slate-400 border-2 hover:border-white hover:cursor-pointer rounded-lg text-white"
+        >
           <FcGoogle className="text-3xl" />
           <button>Вхід Google</button>
         </div>
