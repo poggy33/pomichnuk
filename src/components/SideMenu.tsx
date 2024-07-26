131231a59302de6c0de883db386331ea6129ceca
poggy33 <33poggy33@gmail.com>
2024-07-26 08:39:23 +0100

    update sideMenu Button3

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -15,7 +15,7 @@ function SideMenuItem() {
     <div className="relative">
       <button
         id="dropdownRightEndButton"
-        className="text-white focus:ring-4 focus:outline-none focus:ring-blue-100 rounded-lg text-xl px-3 py-2.5 text-center inline-flex items-center border-2 bg-slate-400 hover:border-white"
+        className="text-white focus:ring-4 focus:outline-none focus:ring-blue-100 rounded-lg text-xl px-3 py-2.5 text-center inline-flex items-center border-2 bg-slate-300 hover:border-white"
         type="button"
         onClick={toggleDropdown}
       >
@@ -41,16 +41,16 @@ function SideMenuItem() {
       {isOpen && (
         <div
           id="dropdownRightEnd"
-          className="z-10 absolute mt-2 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-slate-400"
+          className="z-10 absolute mt-2 bg-slate-300 divide-y divide-gray-100 rounded-lg shadow w-44 "
         >
           <ul
-            className="py-2 text-sm text-gray-700 dark:text-gray-100"
+            className="py-3 text-sm text-white"
             aria-labelledby="dropdownRightEndButton"
           >
             <li onClick={toggleDropdown}>
               <Link
                 href="/userposts"
-                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-slate-500 dark:hover:text-white"
+                className="block px-4 py-3 hover:bg-slate-400"
               >
                 Мої оголошення
               </Link>
@@ -58,7 +58,7 @@ function SideMenuItem() {
             <li onClick={toggleDropdown}>
               <Link
                 href="/about"
-                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-slate-500 dark:hover:text-white"
+                className="block px-4 py-3 hover:bg-slate-400"
               >
                 Про нас
               </Link>
