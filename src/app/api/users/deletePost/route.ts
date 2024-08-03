003e3963524192ea3f4061ab6adb25cc6092423c
poggy33 <33poggy33@gmail.com>
2024-08-03 09:09:41 +0100

    delete post update 3

--- src/app/api/users/deletePost/route.ts
+++ src/app/api/users/deletePost/route.ts
@@ -5,48 +5,22 @@ import Rate from "@/models/rateModel"
 
 connect();
 
-// export async function POST(request: NextRequest) {
-// try {
-//     const reqBody = await request.json();
-//     const {_id} = reqBody;   
-//         const updatedPosts = await Post.findOneAndDelete({_id: _id,})
-//         await Rate.deleteMany({whatIsCheckedId: _id})
-//         return NextResponse.json({
-//         message: "Post deleted",
-//         data: updatedPosts,
-//     })
+export async function POST(request: NextRequest) {
+try {
+    const reqBody = await request.json();
+    const {_id} = reqBody;   
+        const updatedPosts = await Post.findOneAndDelete({_id: _id,})
+        // await Rate.deleteMany({whatIsCheckedId: _id})
+        return NextResponse.json({
+        message: "Post deleted",
+        data: updatedPosts,
+    })
     
-//     }
-//  catch (error: any) {
-//         return NextResponse.json({error: error.message},
-//         {status:400})
-//     }
-// };
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-      try {
-        const reqBody = req.body;
-        const { _id } = reqBody;
-  
-        const deletedPost = await Post.findOneAndDelete({ _id });
-        if (!deletedPost) {
-          return res.status(404).json({ error: 'Post not found' });
-        }
-  
-        await Rate.deleteMany({ whatIsCheckedId: _id });
-  
-        return res.status(200).json({
-          message: 'Post deleted',
-          data: deletedPost,
-        });
-      } catch (error: any) {
-        return res.status(400).json({ error: error.message });
-      }
-    } else {
-      // Return a 405 Method Not Allowed if the method is not POST
-      res.setHeader('Allow', ['POST']);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
     }
-  }
+ catch (error: any) {
+        return NextResponse.json({error: error.message},
+        {status:400})
+    }
+};
+
 
